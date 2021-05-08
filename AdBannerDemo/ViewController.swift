f5786fe5c3ee7051d2ce1a68205ac383c385557b
Mark Wang <4a227031@stust.edu.tw>
2021-05-08 20:26:41 +0800

    Initial Commit

---
+++ AdBannerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdBannerDemo
+//
+//  Created by Wang Sheng Ping on 2021/5/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
